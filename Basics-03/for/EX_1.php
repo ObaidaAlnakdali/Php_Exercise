9fae9b1b32dd25339cde309ca85ef2f38246dcff
obaida <o.alnakdali@gmail.com>
2022-02-28 10:57:37 +0200

    basics3 for Ex1

--- Basics-03/for/EX_1.php
+++ Basics-03/for/EX_1.php
@@ -1,3 +1,12 @@
 <?php
-
+    function sum($n){ 
+        $factorial = 0;
+        $n = $n.'';
+        for ($i=0; $i < strlen($n); $i++) { 
+            $factorial += $n[$i];
+        }
+        echo "$factorial\n";
+    }
+    sum(12345); 
+    sum(9999); 
 ?>
